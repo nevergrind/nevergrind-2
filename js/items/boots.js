5740dd20e7aa68b735e874c77b5732339b8077dd
maelfyn <joemattleonard@gmail.com>
2019-09-23 01:17:51 -0400

    Added boots, legs

--- js/items/boots.js
+++ js/items/boots.js
@@ -3,10 +3,99 @@ var boots = {
 		slots: ['feet'],
 		armorType: 'cloth',
 	},
-	normal: [
-		{
-
-		}
+	normal: [{
+			name: 'Boots',
+			min: 1,
+			max: 4,
+			itemLevel: 1,
+		}, {
+			name: 'Slippers',
+			min: 4,
+			max: 7,
+			itemLevel: 6,
+		}, {
+			name: 'Light Boots',
+			min: 6,
+			max: 8,
+			itemLevel: 14,
+		}, {
+			name: 'Khussa',
+			min: 9,
+			max: 12,
+			itemLevel: 20,
+		}, {
+			name: 'Heavy Boots',
+			min: 7,
+			max: 10,
+			itemLevel: 5,
+			armorType: 'leather',
+		}, {
+			name: 'Leather Boots',
+			min: 13,
+			max: 17,
+			itemLevel: 12,
+			armorType: 'leather',
+		}, {
+			name: 'Sharkskin Boots',
+			min: 18,
+			max: 21,
+			itemLevel: 20,
+			armorType: 'leather',
+		}, {
+			name: 'Bear-hide Boots',
+			min: 22,
+			max: 25,
+			itemLevel: 24,
+			armorType: 'leather',
+		}, {
+			name: 'Chain Boots',
+			min: 19,
+			max: 22,
+			itemLevel: 15,
+			armorType: 'mail',
+		}, {
+			name: 'Scaled Boots',
+			min: 23,
+			max: 26,
+			itemLevel: 21,
+			armorType: 'mail',
+		}, {
+			name: 'Sovereign Boots',
+			min: 27,
+			max: 32,
+			itemLevel: 25,
+			armorType: 'mail',
+		}, {
+			name: 'Blackened Boots',
+			min: 33,
+			max: 37,
+			itemLevel: 28,
+			armorType: 'mail',
+		}, {
+			name: 'Steel Boots',
+			min: 25,
+			max: 29,
+			itemLevel: 20,
+			armorType: 'plate',
+		}, {
+			name: 'Plated Boots',
+			min: 32,
+			max: 36,
+			itemLevel: 25,
+			armorType: 'plate',
+		}, {
+			name: 'Sabatons',
+			min: 39,
+			max: 44,
+			itemLevel: 29,
+			armorType: 'plate',
+		}, {
+			name: 'Cobalt Boots',
+			min: 47,
+			max: 50,
+			itemLevel: 33,
+			armorType: 'plate',
+		},
 	],
 	unique: [
 
--- js/items/legs.js
+++ js/items/legs.js
@@ -3,10 +3,99 @@ var legs = {
 		slots: ['legs'],
 		armorType: 'cloth',
 	},
-	normal: [
-		{
-
-		}
+	normal: [{
+			name: 'Pants',
+			min: 7,
+			max: 12,
+			itemLevel: 1,
+		}, {
+			name: 'Cotton Pants',
+			min: 13,
+			max: 16,
+			itemLevel: 5,
+		}, {
+			name: 'Linen Pants',
+			min: 18,
+			max: 21,
+			itemLevel: 12,
+		}, {
+			name: 'Silk Leggings',
+			min: 22,
+			max: 25,
+			itemLevel: 20,
+		}, {
+			name: 'Fur Pants',
+			min: 14,
+			max: 18,
+			itemLevel: 5,
+			armorType: 'leather',
+		}, {
+			name: 'Wolf-Hide Pants',
+			min: 25,
+			max: 29,
+			itemLevel: 10,
+			armorType: 'leather',
+		}, {
+			name: 'Sharkskin Legs',
+			min: 35,
+			max: 42,
+			itemLevel: 17,
+			armorType: 'leather',
+		}, {
+			name: 'Studded Legs',
+			min: 45,
+			max: 50,
+			itemLevel: 21,
+			armorType: 'leather',
+		}, {
+			name: 'Scaled Legs',
+			min: 37,
+			max: 44,
+			itemLevel: 15,
+			armorType: 'mail',
+		}, {
+			name: 'Chausses',
+			min: 47,
+			max: 55,
+			itemLevel: 21,
+			armorType: 'mail',
+		}, {
+			name: 'Kusazuri',
+			min: 58,
+			max: 66,
+			itemLevel: 25,
+			armorType: 'mail',
+		}, {
+			name: 'Poleyn',
+			min: 69,
+			max: 75,
+			itemLevel: 29,
+			armorType: 'mail',
+		}, {
+			name: 'Cobalt Legplates',
+			min: 49,
+			max: 56,
+			itemLevel: 20,
+			armorType: 'plate',
+		}, {
+			name: 'Iron Legplates',
+			min: 65,
+			max: 74,
+			itemLevel: 25,
+			armorType: 'plate',
+		}, {
+			name: 'Mithril Legplates',
+			min: 78,
+			max: 87,
+			itemLevel: 28,
+			armorType: 'plate',
+		}, {
+			name: 'Royal Legplates',
+			min: 89,
+			max: 100,
+			itemLevel: 33,
+			armorType: 'plate',
+		},
 	],
 	unique: [
 
