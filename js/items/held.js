2b78a65be6cbe6d29b1826c881ac58b21416d0a2
maelfyn <joemattleonard@gmail.com>
2019-09-27 17:32:04 -0400

    Finished basic client-side items

---
+++ js/items/held.js
@@ -0,0 +1,60 @@
+var held = {
+	base: {
+		slots: ['primary'],
+		weaponSkill: 'One-hand Blunt',
+		itemType: 'held',
+	},
+	normal: [{
+			name: 'Stein',
+			minDamage: 1,
+			maxDamage: 4,
+			speed: 3.6, // 3.05
+			itemLevel: 1,
+		}, {
+			name: 'Dark Orb',
+			minDamage: 3,
+			maxDamage: 13,
+			speed: 3.5, // 4.57
+			itemLevel: 5,
+		}, {
+			name: 'Ancient Tome',
+			minDamage: 5,
+			maxDamage: 16,
+			speed: 3.4, // 6.17
+			itemLevel: 10,
+		}, {
+			name: 'Totem',
+			minDamage: 7,
+			maxDamage: 27,
+			speed: 3.8, // 8.94
+			itemLevel: 15,
+		}, {
+			name: 'Crystal Ball',
+			minDamage: 9,
+			maxDamage: 38,
+			speed: 3.7, // 12.7
+			itemLevel: 20,
+		}, {
+			name: 'Skull',
+			minDamage: 11,
+			maxDamage: 48,
+			speed: 3.4, // 17.35
+			itemLevel: 25,
+		}, {
+			name: 'Emblazoned Orb',
+			minDamage: 16,
+			maxDamage: 68,
+			speed: 3.6, // 23.3
+			itemLevel: 30,
+		}, {
+			name: 'Idol',
+			minDamage: 19,
+			maxDamage: 78,
+			speed: 3.3, // 29.39
+			itemLevel: 35,
+		},
+	],
+	unique: [
+
+	]
+}
